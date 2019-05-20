a3254b9da5ec7cdcc29d5a279205cb58fd8a3797
Bauldini <bauldini@users.noreply.github.com>
2019-05-20 15:43:53 -0400

    Update EmotetURL_Parse.py

--- EmotetURL_Parse.py
+++ EmotetURL_Parse.py
@@ -3,23 +3,23 @@ import json
 import os
 
 
-API_KEY = 'Enter your API Key Here'
-pulse_id= 'Enter Your Pulse ID Here'
+API_KEY = ''
+pulse_id= ''
 OTX_SERVER = 'https://otx.alienvault.com'
 
 otx = OTXv2(API_KEY, server=OTX_SERVER)
-x = "urls.txt"
-
+x = "/root/Emotet/urls.txt"
+#y = "fqdn.txt"
 
 
 def get_url():
-    #print 'Getting indicators for pulse:'
-    os.system('echo Getting IOCs from Pulse >> Emotet.log')
+    print 'Getting indicators for Emotet URL pulse:'
+    os.system('echo Getting IOCs from Emotet URL Pulse $(date +%Y-%m-%d_%H:%M:%S) >> /root/Emotet/EmotetURL.log')
     indicators = otx.get_pulse_indicators(pulse_id=pulse_id)
-    json_out = open('IOC_URL.json', 'a+')
+    json_out = open('/root/Emotet/IOC_URL.json', 'a+')
     json_out.write('[')
     for indicator in indicators:
-        file = open("urls.txt", 'a')
+        file = open("/root/Emotet/urls.txt", 'a')
         file.write(indicator['indicator'])
         file.write("\n")
         file.close()
@@ -27,24 +27,23 @@ def get_url():
 
 def outurl_json(x):
     with open(x , 'r') as in_file:
-        json_out = open('IOC_URL.json', 'a+')
+        json_out = open('/root/Emotet/IOC_URL.json', 'a+')
         for line in in_file:
             obj = {u"type": "URL", u"indicator":  line.rstrip()}
             json_out.write(json.dumps(obj, indent=9))
             json_out.write(',\n')
         json_out.write(']')
         json_out.close()
-        os.system('head -n -2 IOC_URL.json | sed -e "\$a }\n" | sed -e "\$a ]" >> IOC_URL2.json')
-
+        os.system('head -n -2 /root/Emotet/IOC_URL.json | sed -e "\$a }\n" | sed -e "\$a ]" >> /root/Emotet/IOC_URL2.json')
 
 def update_pulse():
-    #print 'Updating indicators for pulse:'
-    os.system('echo Updating Pulse >> Emotet.log')
-    with open('IOC_URL2.json', 'r') as data_file:
+    print 'Updating indicators for URL pulse:'
+    os.system('echo Updating Emotet URL Pulse $(date +%Y-%m-%d_%H:%M:%S) >> /root/Emotet/EmotetURL.log')
+    with open('/root/Emotet/IOC_URL2.json', 'r') as data_file:
         data = json.load(data_file)
-		response = otx.replace_pulse_indicators(pulse_id, data)
-        os.system('echo Done at $(date +%Y-%m-%d_%H:%M:%S) >> Emotet.log')
-        print 'Response: ' + str(response)
+	response = otx.replace_pulse_indicators(pulse_id, data)
+        os.system('echo Done Updating Emotet URL pulse at $(date +%Y-%m-%d_%H:%M:%S) >> /root/Emotet/EmotetURL.log')
+        #print 'Response: ' + str(response)
 
 
 get_url()
